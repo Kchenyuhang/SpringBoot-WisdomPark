a1b9154f2e868a9cb26369430d92808b8320ca8d
zhent1106 <1299088269@qq.com>
2020-05-27 08:52:01 +0800

    一卡通前后端交互

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
   <div id="app">
+    <div id="top-bar">
+
+    </div>
     <div id="nav">
       <!-- 权限管理相关操作 -->
-      <h4 @click="perShow = !perShow" style="cursor: pointer;">Permission</h4>
-      <div v-show="perShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="perShow = !perShow"
+        style="cursor: pointer;"
+      >Permission</h4>
+      <div
+        v-show="perShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/admin"> admin</router-link>
         <router-link to="/role"> role</router-link>
       </div>
       <!-- 配置管理相关操作 -->
-      <h4 @click="conShow = !conShow" style="cursor: pointer;">Configuration</h4>
-      <div v-show="conShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="conShow = !conShow"
+        style="cursor: pointer;"
+      >Configuration</h4>
+      <div
+        v-show="conShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/appVersion"> appVersion</router-link>
         <router-link to="/statement"> statement</router-link>
         <router-link to="/feedback"> feedback</router-link>
       </div>
       <!-- 基础管理相关操作 -->
-      <h4 @click="baseShow = !baseShow" style="cursor: pointer;">Base</h4>
-      <div v-show="baseShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="baseShow = !baseShow"
+        style="cursor: pointer;"
+      >Base</h4>
+      <div
+        v-show="baseShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/tower"> tower</router-link>
         <router-link to="/room"> room</router-link>
         <router-link to="/semester"> semester</router-link>
@@ -25,32 +46,62 @@
         <router-link to="/teacher"> teacher</router-link>
       </div>
       <!-- 账号管理相关操作 -->
-      <h4 @click="accShow = !accShow" style="cursor: pointer;">Account</h4>
-      <div v-show="accShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="accShow = !accShow"
+        style="cursor: pointer;"
+      >Account</h4>
+      <div
+        v-show="accShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/account"> account</router-link>
       </div>
       <!-- 图书管理相关操作 -->
-      <h4 @click="bookShow = !bookShow" style="cursor: pointer;">Book</h4>
-      <div v-show="bookShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="bookShow = !bookShow"
+        style="cursor: pointer;"
+      >Book</h4>
+      <div
+        v-show="bookShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/book"> book</router-link>
         <router-link to="/borrow"> borrow</router-link>
       </div>
       <!-- 一卡通管理相关操作 -->
-      <h4 @click="cardShow = !cardShow" style="cursor: pointer;">Card</h4>
-      <div v-show="cardShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="cardShow = !cardShow"
+        style="cursor: pointer;"
+      >Card</h4>
+      <div
+        v-show="cardShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/card"> card</router-link>
         <router-link to="/order"> order</router-link>
         <router-link to="/reportLoss"> reportLoss</router-link>
       </div>
       <!-- 资讯管理相关操作 -->
-      <h4 @click="infoShow = !infoShow" style="cursor: pointer;">Information</h4>
-      <div v-show="infoShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="infoShow = !infoShow"
+        style="cursor: pointer;"
+      >Information</h4>
+      <div
+        v-show="infoShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/infoType"> infoType</router-link>
         <router-link to="/infoManage"> infoManage</router-link>
       </div>
       <!-- 教务管理相关操作 -->
-      <h4 @click="adminShow = !adminShow" style="cursor: pointer;">administration</h4>
-      <div v-show="adminShow" style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;">
+      <h4
+        @click="adminShow = !adminShow"
+        style="cursor: pointer;"
+      >administration</h4>
+      <div
+        v-show="adminShow"
+        style=" margin-left: 10px;display: flex; flex-direction: column; align-items: flex-start;"
+      >
         <router-link to="/timeTable"> timeTable</router-link>
         <router-link to="/examiination"> examiination</router-link>
       </div>
@@ -84,7 +135,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: 2Avenir, Helvetica, Arial, sans-serif;
+  font-family: 2avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/card/Card.vue
+++ src/views/card/Card.vue
@@ -1,53 +1,75 @@
 <template>
   <el-table
-    :data="tableData"
-    style="width: 100%;margin-top:5%"
+    :data="cardList"
+    style="width: 100%;margin-top:5%;margin-left:-3%"
   >
-    <el-table-column type="expand">
-      <template slot-scope="props">
-        <el-form
-          label-position="left"
-          inline
-          class="demo-table-expand"
-        >
-          <el-form-item label="商品名称">
-            <span>{{ props.row.name }}</span>
-          </el-form-item>
-          <el-form-item label="所属店铺">
-            <span>{{ props.row.shop }}</span>
-          </el-form-item>
-          <el-form-item label="商品 ID">
-            <span>{{ props.row.id }}</span>
-          </el-form-item>
-          <el-form-item label="店铺 ID">
-            <span>{{ props.row.shopId }}</span>
-          </el-form-item>
-          <el-form-item label="商品分类">
-            <span>{{ props.row.category }}</span>
-          </el-form-item>
-          <el-form-item label="店铺地址">
-            <span>{{ props.row.address }}</span>
-          </el-form-item>
-          <el-form-item label="商品描述">
-            <span>{{ props.row.desc }}</span>
-          </el-form-item>
-        </el-form>
+    <el-table-column
+      label="卡号"
+      width="180"
+    >
+      <template slot-scope="scope">
+        <span style="margin-left:-5%">{{ scope.row.cardNumber }}</span>
       </template>
     </el-table-column>
     <el-table-column
-      label="商品 ID"
-      prop="id"
+      label="卡密"
+      width="180"
     >
+      <template slot-scope="scope">
+        <span>{{ scope.row.cardPassword }}</span>
+      </template>
     </el-table-column>
     <el-table-column
-      label="商品名称"
-      prop="name"
+      label="绑定账号"
+      width="180"
     >
+      <template slot-scope="scope">
+        <span>{{ scope.row.jobNumber }}</span>
+      </template>
     </el-table-column>
     <el-table-column
-      label="描述"
-      prop="desc"
+      label="余额"
+      width="180"
     >
+      <template slot-scope="scope">
+        <span>{{ scope.row.cardBalance }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="状态"
+      width="180"
+    >
+      <template slot-scope="scope">
+        {{ scope.row.status}}
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="创建时间"
+      width="180"
+    >
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span>{{ scope.row.gmtCreate }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          type="success"
+          @click="handleEdit(scope.$index, scope.row)"
+        >编辑</el-button>
+        <el-button
+          size="mini"
+          type="primary"
+          @click="handleEdit(scope.$index, scope.row)"
+        >流水账单</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)"
+        >删除</el-button>
+      </template>
     </el-table-column>
   </el-table>
 </template>
@@ -56,60 +78,40 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          id: '12987122',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        },
-        {
-          id: '12987123',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        },
-        {
-          id: '12987125',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        },
-        {
-          id: '12987126',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
+      cardList: [],
+      currentPage: 1,
+      pageSize: 10
+    }
+  },
+  created() {
+    this.getCardAll()
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    },
+    getCardAll() {
+      this.axios({
+        method: 'post',
+        url: 'http://localhost:8080/card/all',
+        data: {
+          currentPage: this.currentPage,
+          pageSize: this.pageSize
         }
-      ]
+      })
+        .then((res) => {
+          this.cardList = res.data.data
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.demo-table-expand {
-  font-size: 0;
-}
-.demo-table-expand label {
-  width: 90px;
-  color: #99a9bf;
-}
-.demo-table-expand .el-form-item {
-  margin-right: 0;
-  margin-bottom: 0;
-  width: 50%;
-}
 </style>
