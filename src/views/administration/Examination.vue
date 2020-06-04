c5a4935a64606a1d63588347dc218821251335e7
flygit <1224178565@qq.com>
2020-06-04 21:25:24 +0800

    save

--- src/views/administration/Examination.vue
+++ src/views/administration/Examination.vue
@@ -14,7 +14,7 @@
     </el-header>
     <!-- 考务管理下方的正文内容 -->
     <el-main>
-      <el-table :data="tableData" border style="width: 100%">
+      <el-table :data="examination" border style="width: 100%">
         <el-table-column v-for="(item, index) in th" :prop="item.prop" :label="item.label" :width="item.width" :key="index">
         </el-table-column>
         <el-table-column label="操作" width="243">
@@ -26,6 +26,18 @@
         </el-table-column>
       </el-table>
     </el-main>
+    <el-header>
+      <div class="block">
+        <el-pagination
+          :hide-on-single-page="isShow"
+          @current-change="changePageIndex"
+          :page-size="8"
+          layout="prev, pager, next"
+          :total="total"
+        >
+        </el-pagination>
+      </div>
+    </el-header>
   </el-container>
 </template>
 
@@ -35,95 +47,68 @@ export default {
   name: 'Examination',
   data() {
     return {
-      tableData: [
-        {
-          semester: '2016-05-03',
-          course: '王小虎',
-          type: '上海',
-          teacher: '普陀区',
-          date: '上海市普陀区金沙江路 1516 弄',
-          area: 200333,
-          createTime: 'sad'
-        },
-        {
-          semester: '2016-05-03',
-          course: '王小虎',
-          type: '上海',
-          teacher: '普陀区',
-          date: '上海市普陀区金沙江路 1516 弄',
-          area: 200333,
-          createTime: 'sad'
-        },
-        {
-          semester: '2016-05-03',
-          course: '王小虎',
-          type: '上海',
-          teacher: '普陀区',
-          date: '上海市普陀区金沙江路 1516 弄',
-          area: 200333,
-          createTime: 'sad'
-        },
-        {
-          semester: '2016-05-03',
-          course: '王小虎',
-          type: '上海',
-          teacher: '普陀区',
-          date: '上海市普陀区金沙江路 1516 弄',
-          area: 200333,
-          createTime: 'sad'
-        },
-        {
-          semester: '2016-05-03',
-          course: '王小虎',
-          type: '上海',
-          teacher: '普陀区',
-          date: '上海市普陀区金沙江路 1516 弄',
-          area: 200333,
-          createTime: 'sad'
-        },
-        {
-          semester: '2016-05-03',
-          course: '王小虎',
-          type: '上海',
-          teacher: '普陀区',
-          date: '上海市普陀区金沙江路 1516 弄',
-          area: 200333,
-          createTime: 'sad'
-        }
-      ],
+      examination: [],
       // 表头数据
       th: [
         {
-          width: 144,
+          width: 184,
           prop: 'semester',
           label: '学期'
         },
-        { width: 144, prop: 'course', label: '课程' },
-        { width: 144, prop: 'type', label: '类型' },
-        { width: 144, prop: 'teacher', label: '监考教师' },
-        { width: 144, prop: 'date', label: '时间' },
-        { width: 144, prop: 'area', label: '地点' },
-        { width: 144, prop: 'createTime', label: '创建时间' }
+        { width: 104, prop: 'subjectName', label: '课程' },
+        { width: 104, prop: 'type', label: '类型' },
+        { width: 138, prop: 'teacherName', label: '监考教师' },
+        { width: 164, prop: 'startTime', label: '时间' },
+        { width: 164, prop: 'area', label: '地点' },
+        { width: 164, prop: 'gmtCreate', label: '创建时间' }
       ],
       // 分页对象
       pageDto: {
         currentPage: 0,
-        pageSize: 10
-      }
+        pageSize: 8
+      },
+      // 总数据量
+      total: null,
+      // 是否显示分页组件
+      isShow: null
     }
   },
   components: {},
   created() {
     this.getAllInfo()
+    this.getTotal()
   },
   mounted() {},
   methods: {
     handleClick(row) {
       console.log(row)
     },
+    /**
+     * 调用接口，获取分页数据
+     */
     async getAllInfo() {
-      let examinationRes = await API.init('./examination/all/page', this.pageDto, 'post')
-      console.log(examinationRes)
+      let examinationRes = await API.init('/examination/all/page', this.pageDto, 'post')
+      this.examination = examinationRes.data
+    },
+    /**
+     * 调用接口，获取考务数据的总数
+     */
+    async getTotal() {
+      let totalRes = await API.init('/examination/count', null, 'get')
+      this.total = totalRes.data
+      // 判断查到的数据条数是否满足分页条件
+      if (this.total <= this.pageDto.pageSize) {
+        this.isShow = false
+      } else {
+        this.isShow = true
+      }
+    },
+    /**
+     * 改变分页页码后执行的方法
+     */
+    changePageIndex(currentPage) {
+      this.pageDto.currentPage = currentPage
+      this.getAllInfo()
     }
   },
   computed: {}
