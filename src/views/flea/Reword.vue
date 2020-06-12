886e358253ae07fda909f7f69b802e5bf8196037
ntt <1914866205@qq.com>
2020-06-12 14:02:08 +0800

    跳蚤市场模块搭建

---
+++ src/views/flea/Reword.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>悬赏</div>
+</template>
+
+<script>
+export default {
+  name: 'Reword',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/flea/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>用户</div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
